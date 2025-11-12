bd59f2240007bb4ed14ff6be7d40c24a4bffaf5d
MDorizon <mdorizon@proton.me>
2025-11-12 09:57:35 +0100

    :sparkles: added base emailAndPassword auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    disableOriginCheck: true,
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
